7e1c68fc4f7ab770f14dd86dc2f94b7bb6942788
Stefany Newman <stefany.dyulgerova@gmail.com>
2017-01-06 17:24:27 -0600

    initial commit

---
+++ src/mathqueen/Problem.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package mathqueen;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Problem {
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
