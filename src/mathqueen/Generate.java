bdfe4029956037d8d132e90022508849806ecb49
Stefany Newman <stefany.dyulgerova@gmail.com>
2017-01-06 18:45:51 -0600

    Added Generate.java

---
+++ src/mathqueen/Generate.java
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ */
+package mathqueen;
+import static java.lang.System.out;
+
+import mathqueen.Problem.Type;
+/**
+ * @author Administrator
+ *
+ */
+public class Generate {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Problem problem = new Problem(12, 12, 10);
+		int x = 0;
+		while(x < problem.getNumProblems())
+		{
+			out.println(problem.getTerm1());
+			out.println("+");
+			out.println(problem.getTerm2());
+			out.println("___");
+			out.println();
+			out.println();
+			
+			x++;
+		}
+		
+	}
+
+}
