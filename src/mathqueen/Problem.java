07038bbcc3f9eaad1147d59ae4b75645ee7ebda7
Stefany Newman <stefany.dyulgerova@gmail.com>
2017-01-06 18:45:29 -0600

    Added Problem and Generate classes

--- src/mathqueen/Problem.java
+++ src/mathqueen/Problem.java
@@ -1,21 +1,46 @@
+
+
 /**
  * 
  */
 package mathqueen;
-
+import static java.lang.System.out;
 /**
  * @author Administrator
  *
  */
 public class Problem {
+
 	
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public enum Type{
+		ADDTION,
+		SUBSTRACTION,
+		MULTIPLICATION,
+		DIVISION,
 	}
-
+	int term1;
+	int term2;
+	int numProblems;
+	Problem(int newTerm1, int newTerm2, int newNumProblems)
+	{
+		 term1 = newTerm1;
+		 term2 = newTerm2;
+		 numProblems = newNumProblems;
+	}
+	public int getNumProblems()
+	{
+		return this.numProblems;
+	}
+	public String showTerms()
+	{
+		return  this.term1 + " " + this.term2;
+	}
+	public int getTerm1()
+	{
+		return  this.term1;
+	}
+	public int getTerm2()
+	{
+		return this.term2;
+	}
 }
