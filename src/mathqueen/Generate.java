f5214beba9d84de55d0d6f885478e547bc6b37c3
Stefany Newman <stefany.dyulgerova@gmail.com>
2017-01-13 19:07:59 -0600

    Added helper methods to the Generate class.

--- src/mathqueen/Generate.java
+++ src/mathqueen/Generate.java
@@ -5,6 +5,8 @@
 package mathqueen;
 import static java.lang.System.out;
 
+import java.util.Random;
+
 import mathqueen.Problem;
 /**
  * @author Administrator
@@ -17,20 +19,37 @@ public class Generate {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Problem problem = new Problem(12, 12, 10);
+		Random rand = new Random();
 		int x = 0;
-		while(x < problem.getNumProblems())
+		while(x < 10)
 		{
-			out.println(problem.getTerm1());
-			out.println(Type.SUBSTRACTION.getValue());
-			out.println(problem.getTerm2());
-			out.println("___");
+			int random1 = rand.nextInt(200);
+			int random2 = rand.nextInt(200);
+			Problem problem = new Problem(random1, random2, 10);
+			subtraction(problem.getTerm1(), problem.getTerm2(), Type.SUBSTRACTION.getValue());
+			out.print("____________________");
 			out.println();
 			out.println();
-			
+			out.println();
+			out.println();
 			x++;
 		}
 		
+	}
+	public static void subtraction(int num1, int num2, String operator)
+	{
+		out.println(highest(num1, num2));
+		out.print(operator);
+		out.println();
+		out.println(lowest(num1, num2));
+	}
+	protected static int highest(int num1, int num2)
+	{
+		return (num1 < num2) ? num2 : num1;
+	}
+	protected static int lowest(int num1, int num2)
+	{
+		return (num1 > num2) ? num2 : num1;
 	}
 
 }
